01f9d24c913994985b162dbcc2696626f3ed831d
Pakjivega <pakjivega@gmail.com>
2021-05-15 19:57:23 +0200

    Pieces with colour

--- src/main/scala/com/pakjivega/chessplayer/Pieces.scala
+++ src/main/scala/com/pakjivega/chessplayer/Pieces.scala
@@ -1,13 +1,13 @@
 package com.pakjivega.chessplayer
 
-sealed abstract class Piece(val Initial: Char, val Value:Int) {
+sealed abstract class Piece(val Initial: Char, val Value:Int, val color: Char = 'N') {
 
 }
 
-class Pawn extends Piece('P', 1){}
-class Bishop extends Piece('B',3)  {}
-class Knight extends Piece('N',3)  {}
-class Rook extends Piece('R',5)  {}
-class Queen extends Piece('Q',9)  {}
-class King extends Piece('K',999)  {}
+class Pawn   extends Piece(Initial = 'P', Value = 1  ) {}
+class Bishop extends Piece(Initial = 'B', Value = 3  ) {}
+class Knight extends Piece(Initial = 'N', Value = 3  ) {}
+class Rook   extends Piece(Initial = 'R', Value = 5  ) {}
+class Queen  extends Piece(Initial = 'Q', Value = 9  ) {}
+class King   extends Piece(Initial = 'K', Value = 999) {}
 
